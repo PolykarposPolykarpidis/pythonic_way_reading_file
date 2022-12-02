e1d9fd9ef920e6a24232ffac72af9a543333f593
PolykarposPolykarpidis <61109950+PolykarposPolykarpidis@users.noreply.github.com>
2022-12-02 21:13:21 +0200

    Add files via upload

---
+++ pythonic_readfile.py
@@ -0,0 +1,21 @@
+def read_a_line_from_file(path):
+    file_obj = open(path, "r")
+    file_obj_iter = iter(file_obj)
+    while True:
+        try:
+            n =  next(file_obj_iter)
+            yield n
+        except StopIteration:
+            file_obj.close()
+            break
+
+
+def main_function(filepath):
+    for line in read_a_line_from_file(filepath):
+            print(line)
+
+
+if '__main__' == __name__:
+    filepath = 'file.txt'
+    main_function(filepath)
+    
